3a229dfb2bc0771d2ee296c043617edb447ed79e
Frans Spijkerman <frans@spijkerman.nl>
2026-04-23 10:41:48 +0200

    try to fix api handler

--- src/handlers/apiHandler.mjs
+++ src/handlers/apiHandler.mjs
@@ -2,13 +2,16 @@ import fs from 'fs'
 import path from 'path'
 import { pathToFileURL } from 'url'
 
-const getSafeApiModulePath = (apiroot, item) => {
+const getSafeApiModulePath = (request, apiroot, item) => {
     const normalizedItem = path.posix.normalize(`/${item || ''}`).replace(/^\/+/, '')
+    request.log.error(`Normalized API path: ${normalizedItem}`)
     const modulePath = path.resolve(apiroot, normalizedItem, 'run.mjs')
+    request.log.error(`Resolved API module path: ${modulePath}`)
     const relativeToApiroot = path.relative(apiroot, modulePath)
     if (relativeToApiroot.startsWith('..') || path.isAbsolute(relativeToApiroot)) {
         return null
     }
+    request.log.error(`Resolved API module path: ${modulePath}`)
     return modulePath
 }
 
@@ -26,25 +29,30 @@ const runModule = async (modulePath, request, reply) => {
 }
 
 const run = (apiroot) => async (request, reply) => {
+    request.log.error(`API root: ${apiroot}`)
+    request.log.error(`Handling API request: ${request.url} with method: ${request.method}`)
+
 
     const split = request.url.split('?')
     const queryParams = new URLSearchParams(split[1] || '')
     const url = split[0]
     const parts = url.split('/').filter(Boolean).slice(1) // Remove 'api' prefix
 
-    let modulePath = getSafeApiModulePath(apiroot, parts.join('/'))
+    let modulePath = getSafeApiModulePath(request, apiroot, parts.join('/'))
     if (!modulePath || !fs.existsSync(modulePath)) {
-        modulePath = getSafeApiModulePath(apiroot, parts.slice(0, -1).join('/'))
+        modulePath = getSafeApiModulePath(request, apiroot, parts.slice(0, -1).join('/'))
         if (!modulePath || !fs.existsSync(modulePath)) {
-            modulePath = getSafeApiModulePath(apiroot, parts.slice(0, -2).join('/'))
+            modulePath = getSafeApiModulePath(request, apiroot, parts.slice(0, -2).join('/'))
             if (!modulePath || !fs.existsSync(modulePath)) {
-                modulePath = getSafeApiModulePath(apiroot, parts.slice(0, -3).join('/'))
+                modulePath = getSafeApiModulePath(request,apiroot, parts.slice(0, -3).join('/'))
             }
         }
     }
     if (!modulePath || !fs.existsSync(modulePath)) {
-        request.log.warn({ url: request.url, method: request.method }, 'Invalid API path')
-        return reply.status(400).send({ message: 'Invalid API path' })
+        // const errorMessage = `API module not found for path: ${request.url} => ${modulePath || 'N/A'}`
+        const errorMessage = `API module not found for path: ${parts} => ${modulePath || 'N/A'}`
+        request.log.warn({ url: request.url, method: request.method }, errorMessage)
+        return reply.status(400).send({ message: errorMessage })
     }
 
     try {
